38834510bb7c2023909c6af230ac36090343701a
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2022-05-10 23:12:16 -0500

    fix moviles

--- catalogo_peliculas/lib/DetailMovieViewController.dart
+++ catalogo_peliculas/lib/DetailMovieViewController.dart
@@ -1,8 +1,5 @@
-import 'dart:html';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'SearchMovieViewController.dart';
 
   class DetailMovieViewControllerState extends StatelessWidget{
 
